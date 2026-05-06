055d5a7633868f93b9f2beae2c2de1871d37b7b8
Rohan <andriamihaja2007rohan@gmail.com>
2026-05-06 21:43:55 +0300

    creation UserModel.php

---
+++ codeIgniter/app/Models/UserModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+
+    protected $allowedFields    = [
+        'name',
+        'email',
+        'password',
+        'role'
+    ];
+
+}
