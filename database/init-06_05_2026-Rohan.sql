a04e73d0dac7664b812f21d3f94f0438d05012fb
Rohan <andriamihaja2007rohan@gmail.com>
2026-05-06 11:25:23 +0300

    Create init-06_05_2026-Rohan.sql

---
+++ database/init-06_05_2026-Rohan.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE regime;
+
+USE regime;
+
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    genre ENUM('Homme', 'Femme') NOT NULL
+);
+
+CREATE TABLE caracteristique (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    idUser INT NOT NULL,
+    age INT NOT NULL,
+    height DECIMAL(10, 2) NOT NULL,
+    weight DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (idUser) REFERENCES user (id)
+);
+
+CREATE TABLE regime (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    description TEXT,
+    prixJournalier DECIMAL(10, 2) NOT NULL, -- Prix de base
+    poidsInfluence DECIMAL(10, 2) NOT NULL, -- Ex: -2.5 (kg) ou +1.5 (kg)
+    dureeInfluence INT,            
+    pourcentageViande DECIMAL(5, 2),
+    pourcentagePoisson DECIMAL(5, 2),
+    pourcentageVolaille DECIMAL(5, 2)
+);
+CREATE TABLE activity (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    description TEXT,
+    poidsInfluence DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE objectif (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE option(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE userOption (
+    idUser INT NOT NULL PRIMARY KEY,
+    idOption INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (idUser) REFERENCES user (id),
+    FOREIGN KEY (idOption) REFERENCES option (id)
+);
+
+CREATE TABLE code(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    montant DECIMAL(10, 2) NOT NULL,
+    isValid BOOLEAN NOT NULL
+);
+
+CREATE TABLE portemonaie (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    idUser INT NOT NULL,
+    montant DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (idUser) REFERENCES user (id)
+);
