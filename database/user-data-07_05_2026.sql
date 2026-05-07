5409695950a96273c0349f7b84aafb805dc6b4e6
Rohan <andriamihaja2007rohan@gmail.com>
2026-05-07 20:51:01 +0300

    ajout caracteristique

--- database/user-data-07_05_2026.sql
+++ database/user-data-07_05_2026.sql
@@ -4,3 +4,8 @@ INSERT INTO user (name, email, password, genre, role) VALUES
 ('Jean Dupont',   'jean@test.fr',   'Admin@1234',  'Homme', 'admin'),
 ('Marie Lambert', 'marie@test.fr',  'Admin@5678',  'Femme', 'admin'),
 ('Thomas Martin', 'thomas@test.fr', 'Client@9012', 'Homme', 'client');
+
+INSERT INTO caracteristique (idUser, age, height, weight) VALUES
+(1, 35, 1.78, 85.50),  -- Jean (admin)
+(2, 28, 1.65, 62.00),  -- Marie (admin)
+(3, 42, 1.80, 95.30);  -- Thomas (client)
