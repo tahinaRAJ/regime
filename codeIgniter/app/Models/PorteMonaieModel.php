4c0615b47e17208dcfc3f1f5f3b9cd96d31b70c0
TahinaRAJ <tahinaraj1235@gmail.com>
2026-05-07 14:39:04 +0300

    fonctions pour le porte_monnaie

--- codeIgniter/app/Models/PorteMonaieModel.php
+++ codeIgniter/app/Models/PorteMonaieModel.php
@@ -14,4 +14,75 @@ class PorteMonaieModel extends Model
         'idUser',
         'montant'
     ];
+
+    public function getSolde($idUser)
+    {
+        $result = $this->selectSum('montant')
+            ->where('idUser', $idUser)
+            ->first();
+
+        $solde = $result['montant'] ?? 0;
+        return $solde;
+    }
+
+    public function DeductSolde($idUser, $montant)
+    {
+        $soldeActuel = $this->getSolde($idUser);
+        $nouveauSolde = $soldeActuel - $montant;
+
+        if ($nouveauSolde < 0) {
+            return false;
+        }
+
+        $inserted = $this->insert([
+            'idUser' => $idUser,
+            'montant' => -$montant
+        ]);
+
+        if ($inserted) {
+            return $nouveauSolde;
+        }
+
+        return false;
+    }
+
+    public function validerCode($nomCode)
+    {
+        $code = $this->db->table('code')
+            ->where('nom', $nomCode)
+            ->where('isValid', 1)
+            ->first();
+
+        if (!$code) {
+            return null;
+        }
+
+        return $code;
+    }
+
+    public function addSoldeByCode($idUser, $nomCode)
+    {
+        $code = $this->validerCode($nomCode);
+
+        if (!$code) {
+            return false;
+        }
+
+        $inserted = $this->insert([
+            'idUser' => $idUser,
+            'montant' => $code->montant
+        ]);
+
+        if ($inserted) {
+            $codeUpdated = $this->db->table('code')
+                ->where('id', $code->id)
+                ->update(['isValid' => 0]);
+
+            if ($codeUpdated) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
