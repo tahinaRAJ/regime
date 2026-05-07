ff70e9daeb8dd8d940ab6e1f40932938268433f2
Rohan <andriamihaja2007rohan@gmail.com>
2026-05-07 20:44:33 +0300

    Create user-data-07_05_2026.sql

---
+++ database/user-data-07_05_2026.sql
@@ -0,0 +1,6 @@
+USE regime;
+
+INSERT INTO user (name, email, password, genre, role) VALUES
+('Jean Dupont',   'jean@test.fr',   'Admin@1234',  'Homme', 'admin'),
+('Marie Lambert', 'marie@test.fr',  'Admin@5678',  'Femme', 'admin'),
+('Thomas Martin', 'thomas@test.fr', 'Client@9012', 'Homme', 'client');
