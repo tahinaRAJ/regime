3cdd246813acbdf87757af464886c72339f0f8d7
Rohan <andriamihaja2007rohan@gmail.com>
2026-05-08 15:40:27 +0300

    crud regime

--- codeIgniter/app/Models/RegimeModel.php
+++ codeIgniter/app/Models/RegimeModel.php
@@ -75,7 +75,7 @@ class RegimeModel extends Model
                 $regime['nomActivite'] = 'Aucune activité associée';
                 $regime['poidsInfluenceActivite'] = 0.0;
             }
-            
+
             $influence = (float) $regime['poidsInfluencefood'] + (float) $regime['poidsInfluenceActivite'];
             $duree = (int) ($regime['dureeInfluencefood']);
             $prixJournalier = (float) ($regime['prixJournalier']);
@@ -101,4 +101,51 @@ class RegimeModel extends Model
 
         return $recommendations;
     }
+
+    public function createRegime(string $nom, string $description, float $prixJournalier, float $poidsInfluencefood, int $dureeInfluencefood, float $poidsInfluenceActivite, int $dureeInfluenceActivite, float $pourcentageViande, float $pourcentagePoisson, float $pourcentageVolaille)
+    {
+        $data = [
+            'nom' => $nom,
+            'description' => $description,
+            'prixJournalier' => $prixJournalier,
+            'poidsInfluencefood' => $poidsInfluencefood,
+            'dureeInfluencefood' => $dureeInfluencefood,
+            'poidsInfluenceActivite' => $poidsInfluenceActivite,
+            'dureeInfluenceActivite' => $dureeInfluenceActivite,
+            'pourcentageViande' => $pourcentageViande,
+            'pourcentagePoisson' => $pourcentagePoisson,
+            'pourcentageVolaille' => $pourcentageVolaille
+        ];
+
+        return $this->insert($data);
+    }
+
+    public function updateRegime(int $id, string $nom, string $description, float $prixJournalier, float $poidsInfluencefood, int $dureeInfluencefood, float $poidsInfluenceActivite, int $dureeInfluenceActivite, float $pourcentageViande, float $pourcentagePoisson, float $pourcentageVolaille)
+    {
+        $data = [
+            'nom' => $nom,
+            'description' => $description,
+            'prixJournalier' => $prixJournalier,
+            'poidsInfluencefood' => $poidsInfluencefood,
+            'dureeInfluencefood' => $dureeInfluencefood,
+            'poidsInfluenceActivite' => $poidsInfluenceActivite,
+            'dureeInfluenceActivite' => $dureeInfluenceActivite,
+            'pourcentageViande' => $pourcentageViande,
+            'pourcentagePoisson' => $pourcentagePoisson,
+            'pourcentageVolaille' => $pourcentageVolaille
+        ];
+
+        return $this->update($id, $data);
+    }
+
+    public function deleteRegime(int $id)
+    {
+        return $this->delete($id);
+    }
+
+    public function getRegimeById(int $id)
+    {
+        return $this->find($id);
+    }
+
 }
